4d9dd659bc687ec40a0f0188de14f12a5a551f40
xinyu-intel <xinyu1.chen@intel.com>
2019-01-19 15:02:43 +0800

    debug overflow

--- Package/src/Normal.h
+++ Package/src/Normal.h
@@ -37,6 +37,7 @@ class Normal {
     double y_kt = yi / (sqrt(vol.h));
     double identity = ((y_kt < 0) ? 1 : 0);
     double tao = 0.01;
+    std::cout << "loss = " << abs(tao - identity) * (y_kt * y_kt) * (-1) << std::endl;
     return abs(tao - identity) * (y_kt * y_kt) * (-1);
   }
 
