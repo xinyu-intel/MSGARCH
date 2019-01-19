46c4ff5b657b4eca290945c637540a44d5024005
xinyu-intel <xinyu1.chen@intel.com>
2019-01-19 13:54:32 +0800

    change MLE to ALS loss

--- Package/src/Normal.h
+++ Package/src/Normal.h
@@ -33,7 +33,11 @@ class Normal {
 
   // returns loglikelihood of a single observation
   double kernel(const volatility& vol, const double& yi) {
-    return lncst - 0.5 * pow(yi, 2) / vol.h - 0.5 * vol.lnh;
+    // return lncst - 0.5 * pow(yi, 2) / vol.h - 0.5 * vol.lnh;
+    double y_kt = yi / (sqrt(vol.h));
+    double identity = ((y_kt < 0) ? (1 : 0));
+    double tao = 0.01;
+    return abs(tao - identity) * (y_kt^2) * (-1);
   }
 
   // returns PDF evaluated at "x"
